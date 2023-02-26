f407a09f10374d7096e9bcce1bc80f401fb46f16
Ryan Migaud <rmigaud@asu.edu>
2023-02-26 12:05:24 -0800

    initial project

---
+++ src/test/java/com/github/rmigaud/learningspringboot/LearningspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rmigaud.learningspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
